9ceecb2a8200d8d10352f3863f4006ffe5622113
Van <singularity@vannio.co.uk>
2016-08-19 09:22:07 +0100

    Update comments showing output

--- complement_numbers.rb
+++ complement_numbers.rb
@@ -1,7 +1,7 @@
 # Write a function that returns a complement of n as a base-10 integer
 
-# 50 => 13
-
 def complement_num(num)
   num.to_s(2).gsub(/[10]/, '1' => 0, '0' => 1).to_i(2)
 end
+
+# complement_num(50) => 13
--- pallindrome.rb
+++ pallindrome.rb
@@ -1,7 +1,5 @@
 # Figure out whether or not any anagram of a string can be a palindrome
 
-# string = "cdefghmnipqrstuvw"
-
 def is_palindrome?(str)
   hash = {}
   str.chars.each { |char| hash[char] ? hash[char] += 1 : hash[char] = 1 }
@@ -9,3 +7,5 @@ def is_palindrome?(str)
   hash.map { |key, val| hash[key] = hash[key] % 2 }
   hash.values.reduce(:+).to_i <= 1
 end
+
+# is_palindrome?('cdefghmnipqrstuvw') => false
